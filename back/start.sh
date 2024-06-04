3aecf42cf98c3580abf38a886b2f009a3f488516
alifrappe <alizee.d@hotmail.fr>
2024-06-04 19:03:45 +0200

    remove all files

--- back/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Appliquer les migrations de la base de données
-python manage.py makemigrations
-python manage.py migrate
-
-# Lancer le serveur 
-python manage.py runserver 0.0.0.0:8000
